3910290f4ffccd5f710a4b603a2599c5668c530b
Connor Oswold <coswoldo@gmail.com>
2019-02-15 13:36:39 -0800

    refactored submit form and implemented planet model

---
+++ models/planet.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Populate = require("../util/autopopulate");
+const Schema = mongoose.Schema;
+
+const PlanetSchema = new Schema({
+  name: { type: String, trim: true, required: true },
+  residents: [{ type: Schema.Types.ObjectId, ref: 'Character' }],
+  created: { type: Date, default: Date.now },
+  image: String,
+  url: String,
+  edited: Date
+});
+
+// Populate
+CharacterSchema.pre('findOne', Populate('residents')).pre('find', Populate('residents'))
+
+module.exports = mongoose.model("Planet", PlanetSchema);
