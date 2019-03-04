c8918d4f16219c0d4d2f87b4fecc6e66356c4bf9
Connor Oswold <coswoldo@gmail.com>
2019-03-03 17:19:32 -0800

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000;
 var exphbs = require('express-handlebars')
 const expressValidator = require('express-validator');
 const bodyParser = require('body-parser');
