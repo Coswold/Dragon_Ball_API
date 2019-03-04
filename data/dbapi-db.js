6fb1439210a5f34a79cfd19bd20da6c5f70b1b3c
Connor Oswold <coswoldo@gmail.com>
2019-03-03 18:02:10 -0800

    fixed again

--- data/dbapi-db.js
+++ data/dbapi-db.js
@@ -2,10 +2,10 @@
 const mongoose = require("mongoose");
 assert = require("assert");
 
-const url = process.env.MONGOLAB_ORANGE_URI || 'mongodb://localhost/dbapi-db';
+const url = process.env.MONGOLAB_ORANGE_URL || 'mongodb://localhost/dbapi-db';
 mongoose.Promise = global.Promise;
 mongoose.connect(
-  "mongodb://localhost/dbapi-db",
+  url,
   { useNewUrlParser: true },
   function(err, db) {
     assert.equal(null, err);
